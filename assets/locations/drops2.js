22929f99ff1011dec922811bd9c3dbcd3490a737
elodin <jon@elod.in>
2025-01-30 00:24:27 -0600

    Adding drops

--- assets/locations/drops2.js
+++ assets/locations/drops2.js
@@ -5,48 +5,48 @@ window.staticLocations.push(
 		lat: 33.6,
 		long: 59.2,
 		category: 'Drops & Veins',
-		color: '#000000',
+		color: '#800080', // Purple
 	},
 	{
 		label: 'Purple OSD drop',
 		lat: 25.5,
 		long: 59.3,
 		category: 'Drops & Veins',
-		color: '#000000',
+		color: '#800080', // Purple
 	},
 	{
 		label: 'Red/25k/50k',
 		lat: 18.3,
 		long: 56.1,
 		category: 'Drops & Veins',
-		color: '#000000',
+		color: '#FF0000', // Red
 	},
 	{
 		label: 'Purple/Red/25k/50k',
 		lat: 22.2,
 		long: 46,
 		category: 'Drops & Veins',
-		color: '#000000',
+		color: '#800080', // Purple
 	},
 	{
 		label: 'Red/Yellow/25k/50k',
 		lat: 26.8,
 		long: 53.1,
 		category: 'Drops & Veins',
-		color: '#000000',
+		color: '#FF0000', // Red
 	},
 	{
 		label: 'Red/Yellow/25k/50k',
 		lat: 30.8,
 		long: 52.7,
 		category: 'Drops & Veins',
-		color: '#000000',
+		color: '#FF0000', // Red
 	},
 	{
 		label: 'Yellow/Blue/10k',
 		lat: 33,
 		long: 46.8,
 		category: 'Drops & Veins',
-		color: '#000000',
+		color: '#FFFF00', // Yellow
 	}
 );
